950120587269651a55aa93c41f62b20178bf17ba
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2019-07-31 23:39:26 +0530

    Introduced functions inside code

--- S1Q2_multiplicationtable17.py
+++ S1Q2_multiplicationtable17.py
@@ -1,8 +1,17 @@
-#print multiplication table 17
-a=17
-for i in range(1,11):
-    b=a*i
-    print(a,"*",i,"=",b)
+"""
+print multiplication table 17
+"""
+def multiplication_table():
+    """
+    Using for loop to print multiplication table till 10
+    Input: 17
+    """
+    a=17
+    for i in range(1,11):
+        b=a*i
+        print(a, "*" , i , "=" , b)
+#main starts here
+multiplication_table()
 
 
 
