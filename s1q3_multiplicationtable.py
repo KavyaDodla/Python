d7e8d79ddd9a62f862ca8a9c03c059cc456861a4
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2019-08-01 00:17:34 +0530

    Added functions

--- s1q3_multiplicationtable.py
+++ s1q3_multiplicationtable.py
@@ -1,10 +1,38 @@
+"""
+print the multiplication table of any number desired by the user
+"""
+
 def multiplication_table(a):
-    for i in range(1, 11):
+    """
+    param required:a
+    use for loop to print out first 10 multiplication results
+    """
+    for i in range(1,11):
         b = a * i
-        print(a, "*", i, "=", b)
-a=(input)("What is the number you wish to see multiplication table for?")
-a=int(a)
-if a>0:
+        print(a, "*" , i , "=" , b)
+
+def number():
+    """
+    get number for which multiplication table needs to be printed
+    input:user input
+    convert input to integer
+    execute program only if input is more than 0.return value of user input
+    output:return user input
+    """
+    a=int(input("Please enter the number for multiplication table"))
+    if a>0:
+        return a
+    else:
+        print("Please enter valid input")
+
+def main():
+    a=number()
     multiplication_table(a)
-else:
-    print("Please enter valid input")
+#main starts here
+main()
+
+
+
+
+
+
