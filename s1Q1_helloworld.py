bb1a7f57a9d9951fa7b41a6cc0ee360ab68e752f
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2019-07-31 23:29:05 +0530

    Modified code by using functions

--- s1Q1_helloworld.py
+++ s1Q1_helloworld.py
@@ -1,2 +1,26 @@
-Name=input("Please enter your name")
-print("Hello",Name,"!!!")
+"""
+prompt for user’s name and then wish the user by saying Hello followed by his name
+"""
+def get_user_name():
+    """
+        Prompt the user to enter his name
+        Input : None
+        Output : Return the user input as a string
+        """
+    Name=str(input("Please enter your name"))
+    return Name
+def say_hello(user):
+    """
+    Say hello to the user
+    Input: Name from function (get_user_name)=user
+    Output:print hello to the user
+    """
+    print("Hello", user, "!!!")
+def main():
+    """
+    Main function of the program
+    """
+    user=get_user_name()
+    say_hello(user)
+#main starts from here
+main()
