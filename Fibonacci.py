16fe33021d90c7b7a3dae079a198f9d8720070a6
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2019-08-08 16:14:04 +0530

    Fibonnaci_customized.py
    My previous code was working fine if user needed all positions like 2,4(even basically). But if user needed to print 5,7etc it was not giving correct out put. Now I modified my entire code to give out put for even fibonacci odd fibannoci and all fibaonnaci to the number positions they need

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,21 +1,104 @@
 """
-Print out forst 10 fibonacci numbers
+Based on the user input print either even or odd or all fibonacci numbers
 """
-def logic():
-    a=1
-    b=1
+def fib(fib_count):
+    """
+    input:count
+    :param fib_count:
+    :return:
+    """
+    a = 1
+    b = 1
+    fib=[1,1]
     counter=0
-    position = int(input("Please enter how many fibonacci numbers you require"))
-    repeat=((position-2)/2) # since a and b are defined at first,if user wants 10 fibonacci numbers we only require 8 more.Each time we get two numbers.so it would be half of 8
-    print("", a , "\n" , b)
-    while counter<(repeat):
-        a=a+b
-        b=a+b
-        counter=counter+1
-        print("", a, "\n", b)
-# main starts here
-logic()
+    while fib.__len__()<fib_count:
+        res = a+b
+        a=b
+        b=res
+        fib.append(res)
+
+    print("All Fibonacci numbers : ")
+    for f in fib:
+        print(f)
+
+def fib_even(fib_type, fib_count):
+    print("inside")
+
+    a = 1
+    b = 1
+    fib=[]
+    counter=0
+    while fib.__len__()<fib_count:
+        res = a+b
+        a=b
+        b=res
+        if fib_type == "even":
+            if res % 2 == 0:
+                fib.append(res)
+
+    print("Even Fibonacci numbers : ")
+    for f in fib:
+        if f % 2 == 0:
+            print(f)
 
 
+def fib_odd(fib_type, fib_count):
+    print("inside")
+
+    a = 1
+    b = 1
+    fib=[]
+    counter=0
+    while fib.__len__()<fib_count:
+        res = a+b
+        a=b
+        b=res
+        if fib_type == "odd":
+            if res % 2 != 0:
+                fib.append(res)
+    print("Odd Fibonacci numbers : ")
+    for f in fib:
+        if f % 2 != 0:
+            print(f)
+
+def main():
+    fib_type=str(input("Even/Odd/all fibonnaci numbers you need?"))
+    fib_count = int(input("How many " + fib_type + " numbers you need?"))
+    if fib_type == "even":
+        fib_even(fib_type, fib_count)
+    elif fib_type == "odd":
+        fib_even(fib_type, fib_count)
+    else:
+        fib(fib_count)
+
+main()
 
 
+# def fib(fib_type, fib_count):
+#     print("inside")
+#
+#     a = 1
+#     b = 1
+#     fib=[]
+#     counter=0
+#     while fib.__len__()<fib_count:
+#         res = a+b
+#         a=b
+#         b=res
+#         if fib_type == "even":
+#             if res % 2 == 0:
+#                 fib.append(res)
+#         if fib_type == "odd":
+#             if res % 2 != 0:
+#                 fib.append(res)
+#
+#     if fib_type == "even":
+#         print("Even Fibonacci numbers : ")
+#         for f in fib:
+#             if f % 2 == 0:
+#                 print(f)
+#     elif fib_type == "odd":
+#         print("Odd Fibonacci numbers : ")
+#         for f in fib:
+#             if f % 2 != 0:
+#                 print(f)
