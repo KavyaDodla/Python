6b30b89ab82294779760598ef8aec469f179dd51
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2019-08-15 21:38:44 +0530

    Create Coding Exam Question 11_fibonacci_even.py

---
+++ Coding Exam Question 11_fibonacci_even.py
@@ -0,0 +1,25 @@
+
+def logic():
+    a = 1
+    b = 1
+    list = []
+    length=0
+    while length<12:
+        res=a+b
+        a = b
+        b = res
+        if b%2==0:
+            list.append(b)
+            length=0
+            for i in list:
+                length+=1
+    else:
+        print(list)
+
+logic()
+
+
+
+
+
+
