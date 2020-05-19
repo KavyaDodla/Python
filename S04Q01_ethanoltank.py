ed7b07ad215b414800612225f86ac11eee4f6798
KavyaDodla <53465053+KavyaDodla@users.noreply.github.com>
2020-05-19 23:14:43 +0530

    Update S04Q01_ethanoltank.py

--- S04Q01_ethanoltank.py
+++ S04Q01_ethanoltank.py
@@ -1,30 +1,29 @@
-"""
-When the tank is more than 80% full, it should raise an alarm to close the valve
-When the tank is less than 20% full, it should send an SMS to buy more liquid.
-"""
-def prompt_user():
-    """
-    Input:user enter data for tank capacity and ethanol level
-    output:fill rate
-    return fill rate
-    """
-    tank_capacity=int(input("What is the total tank capacity"))
-    ethanol_level=int(input("Please enter the reading of ethanol in tank"))
-    fill_rate=(ethanol_level*100)/tank_capacity
-    return fill_rate
+def get_data():
+    tank_capacity=int(input("please enter the total tank capacity"))
+    current_capacity=int(input(" Please enter the curent level of ethanol in the tank"))
+    return tank_capacity,current_capacity
+    
+def alarm(percentage):
+    print ("Please close the valve.Your tank is ",percentage,"full")
 
-def tank_check():
-    """
-    input: fill rate from prompt_user function
-    out put: print statement-When the tank is more than 80% full, it should raise an alarm to close the valve.
-    When the tank is less than 20% full, it should send an SMS to buy more liquid.
+def sms(percentage):
+    print("Please buy more ethanol. Your ethanol percenatage is",percentage)
 
-    """
-    percentage=prompt_user()
-    if percentage>80:
-        print("Your tank is",percentage,"full. Please close the valve")
-    elif percentage<20:
-        print("Your tank is", percentage, "Please send ans SMS to user to buy more liquid")
-# main starts from here
-tank_check()
+def check_percentage():
+    tank_capacity,current_capacity=get_data()
+    percentage=(current_capacity*100)/tank_capacity
+    if percentage>=80:
+        alarm(percentage)
+    elif percentage<=20:
+        sms(percentage)
+    
+def main():
+    check_percentage()
+    
+main()
+    
 
+
+    
+
+    
